495f52d4d8008453186ddb44aedfad8f4f5a9058
AlexandrosPoulis <166614376+AlexandrosPoulis@users.noreply.github.com>
2026-03-03 10:26:36 +0100

    Update webserver.py to improve port handling
    Refactor port assignment to use environment variable directly.

--- webserver.py
+++ webserver.py
@@ -2,8 +2,6 @@ from flask import Flask
 from threading import Thread
 import os
 
-const port = process.env.PORT || 4000 
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,10 +10,12 @@ def home():
 
 
 def run():
+    port = int(os.environ.get("PORT"))  # DO NOT put default
     app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
-    t.daemon = True
+    #t.daemon = True
     t.start()
+
 
