3287212df78e7d1aa8d0d9a5ae54046a9dd48f65
AlexandrosPoulis <166614376+AlexandrosPoulis@users.noreply.github.com>
2026-03-03 10:19:43 +0100

    Change port to use environment variable or default

--- webserver.py
+++ webserver.py
@@ -2,6 +2,8 @@ from flask import Flask
 from threading import Thread
 import os
 
+const port = process.env.PORT || 4000 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,9 +12,10 @@ def home():
 
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
     t.daemon = True
     t.start()
+
