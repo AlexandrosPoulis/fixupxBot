da7a0b54b73da3ddb9f3d2ad46d3ca6aaa420c21
AlexandrosPoulis <166614376+AlexandrosPoulis@users.noreply.github.com>
2026-03-22 12:08:20 +0100

    Update Flask app response and port configuration

--- webserver.py
+++ webserver.py
@@ -1,21 +1,16 @@
 from flask import Flask
 from threading import Thread
-import os
 
-app = Flask(__name__)
+app = Flask('')
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Discord bot ok"
-
+    return "Discord bot is running!"
 
 def run():
-    port = int(os.environ.get("PORT"))  # DO NOT put default
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     t = Thread(target=run)
     #t.daemon = True
     t.start()
-
-
